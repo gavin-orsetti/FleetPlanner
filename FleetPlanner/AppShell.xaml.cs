347fbdc3d32c60e9251231b6cc929d99a2c1569b
gavin_orsetti <orsetti.gavin@gmail.com>
2024-01-12 12:50:13 -0800

    Added switch to swap between dark and light theme and made it default to using the same theme as the system theme.
    Continued to flesh out colors and styling.

--- FleetPlanner/AppShell.xaml.cs
+++ FleetPlanner/AppShell.xaml.cs
@@ -13,5 +13,27 @@ public partial class AppShell : Shell
         // Create New Fleet
         Routing.RegisterRoute( Routes.MyFleets_NewFleet_PageName, Routes.MyFleets_NewFleet_PageType );
         #endregion Fleet Routes
+
+        #region Theme Settings
+        // Gets the user's system theme and sets the app theme to match
+        App.Current.UserAppTheme = Application.Current.RequestedTheme;
+
+        // Sets the Light/Dark theme switch to match the selected theme
+        themeSwitch.IsToggled = App.Current.RequestedTheme == AppTheme.Dark;
+        App.Current.RequestedThemeChanged += ( s, e ) =>
+        {
+            themeSwitch.IsToggled = App.Current.RequestedTheme == AppTheme.Dark;
+        };
+        #endregion ThemeSettings
+
+    }
+    /// <summary>
+    /// Swaps the theme when the switch is flipped.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e">The boolean value that represents the switch state (true == dark mode, false == light mode) </param>
+    private void ThemeToggled( object sender, ToggledEventArgs e )
+    {
+        App.Current.UserAppTheme = e.Value ? AppTheme.Dark : AppTheme.Light;
     }
 }
