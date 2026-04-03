4e326008a0d501dc44aa08da7b62e41cdf0a2c39
Gavin Orsetti <87047160+gavin-orsetti@users.noreply.github.com>
2026-04-03 21:36:26 +0000

    fix: strip MauiProgram to bare minimum for launch diagnosis

--- FleetPlanner/App.xaml.cs
+++ FleetPlanner/App.xaml.cs
@@ -2,16 +2,9 @@ namespace FleetPlanner;
 
 public partial class App : Application
 {
-    private readonly AppShell _shell;
-
     public App(AppShell shell)
     {
         InitializeComponent();
-        _shell = shell;
-    }
-
-    protected override Window CreateWindow(IActivationState? activationState)
-    {
-        return new Window(_shell);
+        MainPage = shell;
     }
 }
