45106ae44ba47bc9f9e67a15e194aca03f8b71c6
Gavin Orsetti <87047160+gavin-orsetti@users.noreply.github.com>
2026-04-03 23:20:49 +0000

    fix: remove crash logger and Android crash handler, restore clean MainActivity

--- FleetPlanner/Platforms/Android/MainActivity.cs
+++ FleetPlanner/Platforms/Android/MainActivity.cs
@@ -1,16 +1,9 @@
-﻿using Android.App;
+using Android.App;
 using Android.Content.PM;
-using Android.OS;
-using FleetPlanner.Platforms.Android;
 
 namespace FleetPlanner;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
-    protected override void OnCreate(Bundle? savedInstanceState)
-    {
-        AndroidCrashHandler.Register();
-        base.OnCreate(savedInstanceState);
-    }
 }
