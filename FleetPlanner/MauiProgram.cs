d6a30ac78c99abc514924c16b072adc388891bbc
Gavin Orsetti <87047160+gavin-orsetti@users.noreply.github.com>
2026-04-03 22:27:27 +0000

    fix: remove SkiaSharp and LiveCharts to isolate native crash

--- FleetPlanner/MauiProgram.cs
+++ FleetPlanner/MauiProgram.cs
@@ -1,19 +1,12 @@
-using LiveChartsCore.SkiaSharpView.Maui;
-using SkiaSharp.Views.Maui.Controls.Hosting;
-
 namespace FleetPlanner;
 
 public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
     {
-        SQLitePCL.Batteries_V2.Init();
-
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
-            .UseSkiaSharp()
-            .UseLiveCharts()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
